a03a2cab3e668cdffadff5afa3d876014f2506a0
Denis <demaz@bk.ru>
2022-07-15 11:35:17 +0700

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        int clientOs = 0;
+
+        switch (clientOs) {
+            case 0:
+                System.out.println("«Установите версию приложения для iOS по ссылке».");
+                break;
+            case 1:
+                System.out.println("«Установите версию приложения для Android по ссылке».");
+                break;
+            default:
+                System.out.println("Ваша операционная система не опознана, обратитесь в тех. поддержку!");
+        }
     }
 }
