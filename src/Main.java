5caafa253f3da5881c31b21976abbf3bed78cde6
Denis <demaz@bk.ru>
2022-07-15 12:08:57 +0700

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -8,13 +9,37 @@ public class Main {
 
         switch (clientOs) {
             case 0:
-                System.out.println("«Установите версию приложения для iOS по ссылке».");
+                System.out.println("«Установите версию приложения для iOS по ссылке».\n");
                 break;
             case 1:
-                System.out.println("«Установите версию приложения для Android по ссылке».");
+                System.out.println("«Установите версию приложения для Android по ссылке».\n");
                 break;
             default:
-                System.out.println("Ваша операционная система не опознана, обратитесь в тех. поддержку!");
+                System.out.println("Ваша операционная система не опознана, обратитесь в тех. поддержку!\n");
         }
+    }
+
+    public static void task2() {
+        int clientOs = 1;
+        int clientYear = 2011;
+
+        boolean isIOS = clientOs == 0;
+        boolean isAndroid = clientOs == 1;
+        boolean isPhoneOld = clientYear < 2015;
+
+        if (isIOS && isPhoneOld) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        }
+        if (isIOS && !isPhoneOld) {
+            System.out.println("«Установите версию приложения для iOS по ссылке».");
+        }
+
+        if (isAndroid && isPhoneOld) {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
+        }
+        if (isAndroid && !isPhoneOld) {
+            System.out.println("«Установите версию приложения для Android по ссылке».");
+        }
+
     }
 }
